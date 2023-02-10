bd8e5682b9b76594b37db7f16e580d88460fa508
miaw88 <miawsupanan08@gmail.com>
2023-02-11 05:23:19 +0700

    more backend - close #208

--- backend/controller/OrderTechController.go
+++ backend/controller/OrderTechController.go
@@ -3,13 +3,26 @@ package controller
 import (
 	"net/http"
 
+	"github.com/asaskevich/govalidator"
 	"github.com/gin-gonic/gin"
 	"github.com/sut65/team11/entity"
+	"gorm.io/gorm"
 )
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////   		   controller OrderTech    		////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+type extendedOrderTech struct {
+	entity.OrderTech
+	Name string
+}
+
+type extendedOrderTechCus struct {
+	entity.OrderTech
+	limits uint
+	Name   string
+}
 
 // POST /OrderTech
 func CreateOrderTech(c *gin.Context) {
@@ -57,17 +70,21 @@ func CreateOrderTech(c *gin.Context) {
 
 	// 15: สร้าง OrderTech
 	pm := entity.OrderTech{
+		Model:      gorm.Model{ID: OrderTech.ID},
 		ORDER:      ORDER,
 		CostDetail: CostDetail, // โยงความสัมพันธ์กับ Entity CostDetail
 		Damage:     Damage,     // โยงความสัมพันธ์กับ Entity Damage
 		Status:     Status,     // โยงความสัมพันธ์กับ Entity Status
 		Technician: technician, // โยงความสัมพันธ์กับ Entity Technician
-		// Reason:     OrderTech.Reason,
-		Solving: OrderTech.Solving,
-		// Date_time:  ORDER.Date_time,
-		TimeOut: OrderTech.TimeOut,
+		Solving:    OrderTech.Solving,
+		TimeOut:    OrderTech.TimeOut,
 
-		ForPaymentStatus: OrderTech.ForPaymentStatus,  //จั๊ดเป็นคนเพิ่ม ใช้ในระบบจั๊ด
+		ForPaymentStatus: OrderTech.ForPaymentStatus, //จั๊ดเป็นคนเพิ่ม ใช้ในระบบจั๊ด
+	}
+
+	if _, err := govalidator.ValidateStruct(pm); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
 	}
 
 	// 13: บันทึก
@@ -82,8 +99,9 @@ func CreateOrderTech(c *gin.Context) {
 // GET /OrderTech
 // List all OrderTechs
 func ListOrderTechs(c *gin.Context) {
-	var OrderTechs []entity.OrderTech
-	if err := entity.DB().Preload("ORDER.CASE").Preload("ORDER.Address.AddressType").Preload("ORDER.Device.Windows").Preload("ORDER.Device.Type").Preload("ORDER.Device.Customer.GENDER").Preload("ORDER.Device.Customer.CAREER").Preload("ORDER.Device.Customer.PREFIX").Preload("Technician.EDUCATE").Preload("Technician.PREFIX").Preload("ORDER.Address.Tambon.District.Province").Preload("ORDER.Device.Windows").Preload("ORDER.Device.Type").Preload("ORDER.Device.Customer.GENDER").Preload("ORDER.Device.Customer.CAREER").Preload("ORDER.Device.Customer.PREFIX").Preload("Technician.GENDER").Preload("CostDetail").Preload("Damage").Preload("Status").Raw("SELECT * FROM order_teches").Find(&OrderTechs).Error; err != nil {
+	var OrderTechs []extendedOrderTech
+	// if err := entity.DB().Raw("SELECT ot.* , t.name, s.status_name, d.damage_name,c.cost FROM order_teches ot JOIN technicians t JOIN statuses s JOIN cost_details c JOIN damages d ON ot.technician_id = t.id AND ot.status_id = s.id AND ot.cost_detail_id = c.id AND ot.damage_id = d.id").Find(&OrderTechs).Error; err != nil {
+	if err := entity.DB().Preload("ORDER.CASE").Preload("ORDER.Address.AddressType").Preload("ORDER.Device.Windows").Preload("ORDER.Device.Type").Preload("ORDER.Device.Customer.GENDER").Preload("ORDER.Device.Customer.CAREER").Preload("ORDER.Device.Customer.PREFIX").Preload("Technician.EDUCATE").Preload("Technician.PREFIX").Preload("ORDER.Address.Tambon.District.Province").Preload("ORDER.Device.Windows").Preload("ORDER.Device.Type").Preload("ORDER.Device.Customer.GENDER").Preload("ORDER.Device.Customer.CAREER").Preload("ORDER.Device.Customer.PREFIX").Preload("Technician.GENDER").Preload("CostDetail").Preload("Damage").Preload("Status").Raw("SELECT ot.* , t.name, s.status_name, d.damage_name,c.cost FROM order_teches ot JOIN technicians t JOIN statuses s JOIN cost_details c JOIN damages d ON ot.technician_id = t.id AND ot.status_id = s.id AND ot.cost_detail_id = c.id AND ot.damage_id = d.id").Find(&OrderTechs).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
@@ -94,9 +112,9 @@ func ListOrderTechs(c *gin.Context) {
 // GET /OrderTech/:id
 // Get OrderTech by id
 func GetOrderTech(c *gin.Context) {
-	var OrderTech entity.OrderTech
+	var OrderTech extendedOrderTechCus
 	id := c.Param("id")
-	if tx := entity.DB().Preload("id = ?", id).First(&OrderTech); tx.RowsAffected == 0 {
+	if tx := entity.DB().Raw("SELECT ot.* , t.name, s.status_name, o.limits , d.damage_name,c.cost FROM order_teches ot JOIN technicians t JOIN orders o JOIN statuses s JOIN cost_details c JOIN damages d ON ot.technician_id = t.id AND ot.order_id = o.id AND ot.status_id = s.id AND ot.cost_detail_id = c.id AND ot.damage_id = d.id WHERE ot.id = ?", id).First(&OrderTech); tx.RowsAffected == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "OrderTech not found"})
 		return
 	}
@@ -104,28 +122,83 @@ func GetOrderTech(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"data": OrderTech})
 }
 
-// PATCH /OrderTech
+func GetOrderTechcus(c *gin.Context) {
+	var OrderTech []extendedOrderTechCus
+	id := c.Param("id")
+	if tx := entity.DB().Raw("SELECT ot.*, c.name FROM order_teches ot JOIN orders o ON ot.order_id = o.id JOIN customers c ON o.customer_id = c.id WHERE ot.id = ?", id).First(&OrderTech); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "OrderTech not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": OrderTech})
+}
+
+
+// PATCH /users
 func UpdateOrderTech(c *gin.Context) {
 	var orderTech entity.OrderTech
-	var newOrderTech entity.OrderTech
+	var status entity.Status
+	var damage entity.Damage
+	var costDetail entity.CostDetail
+	var order entity.ORDER
+	var technician entity.Technician
 
 	if err := c.ShouldBindJSON(&orderTech); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+	var upSolving = orderTech.Solving
+	var upTimeOut = orderTech.TimeOut
 
-	newOrderTech.Solving = orderTech.Solving
-	newOrderTech.TimeOut = orderTech.TimeOut
-	newOrderTech.StatusID = orderTech.StatusID
-	newOrderTech.DamageID = orderTech.DamageID
-	newOrderTech.CostDetailID = orderTech.CostDetailID
+	if tx := entity.DB().Where("id = ?", orderTech.StatusID).First(&status); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "status not found"})
+		return
+	}
 
-	if err := entity.DB().Where("id = ?", orderTech.ID).Updates(&newOrderTech).Error; err != nil {
+	if tx := entity.DB().Where("id = ?", orderTech.DamageID).First(&damage); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "damage tech not found"})
+		return
+	}
+
+	if tx := entity.DB().Where("id = ?", orderTech.CostDetailID).First(&costDetail); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "costDetail not found"})
+		return
+	}
+
+	if tx := entity.DB().Where("id = ?", orderTech.OrderID).First(&order); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "order tech not found"})
+		return
+	}
+
+	if tx := entity.DB().Where("id = ?", orderTech.TechnicianID).First(&technician); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "technician not found"})
+		return
+	}
+	if tx := entity.DB().Where("id = ?", orderTech.ID).First(&orderTech); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "order tech not found"})
+		return
+	}
+
+	up_pt := entity.OrderTech{
+		Model:      gorm.Model{ID: orderTech.ID},
+		Status:     status,
+		Damage:     damage,
+		CostDetail: costDetail,
+		ORDER:      order,
+		Technician: technician,
+		Solving:    upSolving,
+		TimeOut:    upTimeOut,
+	}
+	if _, err := govalidator.ValidateStruct(up_pt); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"data": newOrderTech})
+	if err := entity.DB().Save(&up_pt).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": up_pt})
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
