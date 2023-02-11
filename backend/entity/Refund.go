92e5a115303fb4cc4bb21c69ba172d1287719469
napatsakornboy <fewnapatsakorn@gmail.com>
2023-02-11 18:38:17 +0700

    Validate Cause Refund - close #93

--- backend/entity/Refund.go
+++ backend/entity/Refund.go
@@ -2,7 +2,9 @@ package entity
 
 import (
 	"time"
+	"unicode"
 
+	"github.com/asaskevich/govalidator"
 	"gorm.io/gorm"
 )
 
@@ -22,16 +24,34 @@ type Contact struct {
 
 type Refund struct {
 	gorm.Model
-	Refund_Cause   string
-	Refund_Contact string
+	Refund_Cause   string `valid:"required~กรุณากรอกเหตุผลที่ยกเลิกออเดอร์,isAlpha~กรุณาไม่ใช้ตัวอีกษรพิเศษ,maxstringlength(100)~กรอกเหตุผลไม่เกิน 100 อักษร"`
+	Refund_Contact string `valid:"required~กรุณากรอกช่องทางการคืนเงิน,isAlpha~กรุณาไม่ใช้ตัวอีกษรพิเศษ"`
 	Refund_time    time.Time
 
-	OrderID *uint
-	ORDER	ORDER `gorm:"references:id"`
+	OrderID uint
+	ORDER   ORDER `gorm:"references:id" valid:"-"`
 
-	CauseID *uint
-	Cause   Cause `gorm:"references:id"`
+	CauseID uint
+	Cause   Cause `gorm:"references:id" valid:"-"`
 
-	ContactID *uint
-	Contact   Contact `gorm:"references:id"`
+	ContactID uint
+	Contact   Contact `gorm:"references:id" valid:"-"`
 }
+
+func init() {
+	govalidator.CustomTypeTagMap.Set("isAlpha", func(i interface{}, _ interface{}) bool {
+		field, ok := i.(string)
+		if !ok {
+			return false
+		}
+
+		for _, char := range field {
+			if !unicode.IsLetter(char) && !unicode.IsNumber(char) {
+				return false
+			}
+		}
+		return true
+	})
+
+}
+
