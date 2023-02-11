bc6f3f88ca7724f709cbcf785fa38a79dd41c4a1
Natthawat-CPE <aha0037@gmail.com>
2023-02-11 21:26:46 +0700

    38 - close #38

--- backend/entity/Customer.go
+++ backend/entity/Customer.go
@@ -34,25 +34,26 @@ type Role struct {
 
 type Customer struct {
 	gorm.Model
-	Name    string
-	ID_card string
-	DOB     time.Time
-	Phone   string
+	Name    string	`valid:"required~Name cannot be blank"`
+	ID_card string	`valid:"required~คุณยังไม่ได้กรอก เลขบัตรประชาชน"`
+	DOB     time.Time 
+	Phone   string	`valid:"required~คุณยังไม่ได้กรอก เบอร์มือถือ, matches(^\\d{10}$)~เลขเบอร์โทรศัพท์ต้องมี 10 ตัว"`
+	// Phone   string	`valid:"required~คุณยังไม่ได้กรอก เบอร์มือถือ, matches(^//d{10}$)~เลขเบอร์โทรศัพท์ต้องมี 10 ตัว"`
 
 	GENDER_ID *uint
-	GENDER    Gender `gorm:"references:id"`
+	GENDER    Gender `gorm:"references:id" valid:"-"`
 
 	CAREER_ID *uint
-	CAREER    Career `gorm:"references:id"`
+	CAREER    Career `gorm:"references:id" valid:"-"`
 
 	PREFIX_ID *uint
-	PREFIX    Prefix `gorm:"references:id"`
+	PREFIX    Prefix `gorm:"references:id" valid:"-"`
 
-	Email    string `gorm:"uniqueIndex"`
+	Email    string `gorm:"uniqueIndex" valid:"email~Syntax: Email ไม่ถูกต้อง, required~คุณไม่ได้กรอก Email"`
 	Password string
 
 	ROLE_ID *uint
-	ROLE    Role `gorm:"references:id"`
+	ROLE    Role `gorm:"references:id" valid:"-"`
 
 	Address         []Address         `gorm:"ForeignKey:CustomerID"`
 	Device          []Device          `gorm:"ForeignKey:CustomerID"`
